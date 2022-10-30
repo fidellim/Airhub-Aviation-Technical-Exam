560a85eb99fd76af0f2d1ea23c50032c53f1e3f3
fidellim <lim.fidel@gmail.com>
2022-10-30 15:15:30 +0400

    show popup if login is success and listen to auth changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,29 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker'
 import dayjs from 'dayjs'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, forwardRef } from 'react'
 import { useFormik } from 'formik'
 import { todoValidationSchema } from '../library/form'
 import { addTodo, queryTodos } from '../database'
 import { onSnapshot } from 'firebase/firestore'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from '../library/firebaseConfig'
 import Todo from '../components/Todo'
+import { useNavigate } from 'react-router-dom'
+import Snackbar from '@mui/material/Snackbar'
+import MuiAlert from '@mui/material/Alert'
+
+const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
 
 const Home = () => {
+    let navigate = useNavigate()
     const [todos, setTodos] = useState([])
+    const [open, setOpen] = useState(false)
     const [isChecked, setIsChecked] = useState(false)
+    const [isSuccess, setIsSuccess] = useState(false)
+    const [user, setUser] = useState()
 
     useEffect(() => {
         const unsubscribe = onSnapshot(queryTodos, (querySnapshot) => {
@@ -22,6 +35,27 @@ const Home = () => {
                     return { ...doc.data(), id: doc.id }
                 })
             )
+        })
+
+        return () => unsubscribe()
+    }, [])
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const uid = user.uid
+                // console.log(user.email)
+                setUser(user)
+                setIsSuccess(true)
+                setOpen(true)
+                // ...
+            } else {
+                // User is signed out
+                // ...
+                navigate(`/login`)
+            }
         })
 
         return () => unsubscribe()
@@ -50,8 +84,35 @@ const Home = () => {
         setIsChecked(event.target.checked)
     }
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return
+        }
+
+        setOpen(false)
+    }
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                // Sign-out successful.
+            })
+            .catch((error) => {
+                // An error happened.
+            })
+    }
+
     return (
         <div className="App">
+            <Box>
+                <Button variant="outlined" onClick={handleSignOut}>
+                    Logout
+                </Button>
+                <Typography variant="h1" component="h1">
+                    {user && user.email}
+                </Typography>
+            </Box>
+
             <Typography variant="h1" component="h1">
                 TODO
             </Typography>
@@ -115,6 +176,19 @@ const Home = () => {
                         <Todo key={todo.id} {...todo} />
                     ))}
             </Box>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert
+                    onClose={handleClose}
+                    severity={`${isSuccess ? 'success' : 'error'}`}
+                    sx={{ width: '100%' }}
+                >
+                    {`${
+                        isSuccess
+                            ? 'Login Success!'
+                            : 'Email/password is incorrect. Please try again.'
+                    }`}
+                </Alert>
+            </Snackbar>
         </div>
     )
 }
