b7fdfcb231fb5f21b2772e1921a748dc093062a2
fidellim <lim.fidel@gmail.com>
2022-10-30 16:19:57 +0400

    add path as a parameter to all functions

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,4 @@
-import { db, FIREBASE_COLLECTION } from '../library/firebaseConfig'
+import { db } from '../library/firebaseConfig'
 import {
     addDoc,
     collection,
@@ -9,21 +9,23 @@ import {
     query,
 } from 'firebase/firestore'
 
-export const queryTodos = query(collection(db, FIREBASE_COLLECTION))
-
-export const getTodos = () => {
-    return getDocs(collection(db, FIREBASE_COLLECTION))
+export const queryTodos = (path) => {
+    return query(collection(db, path))
 }
 
-export const addTodo = async (data) => {
-    return await addDoc(collection(db, FIREBASE_COLLECTION), data)
+export const getTodos = (path) => {
+    return getDocs(collection(db, path))
 }
 
-export const deleteTodo = (id) => {
-    return deleteDoc(doc(db, FIREBASE_COLLECTION, id))
+export const addTodo = async (data, path) => {
+    return await addDoc(collection(db, path), data)
 }
 
-export const updateTodo = async (id, data) => {
-    const todoRef = doc(db, FIREBASE_COLLECTION, id)
+export const deleteTodo = (id, path) => {
+    return deleteDoc(doc(db, path, id))
+}
+
+export const updateTodo = async (id, data, path) => {
+    const todoRef = doc(db, path, id)
     return await updateDoc(todoRef, data)
 }
