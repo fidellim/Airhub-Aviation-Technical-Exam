fee0c55ad54ad2f291bd20dcfe3cc2da0bb14e32
fidellim <lim.fidel@gmail.com>
2022-10-30 20:48:57 +0400

    add registration validation schema

--- src/library/form.js
+++ src/library/form.js
@@ -14,6 +14,21 @@ export const loginValidationSchema = yup.object({
     password: yup.string().required('Please enter your password.'),
 })
 
+export const registerValidationSchema = yup.object({
+    email: yup
+        .string()
+        .email('Please enter a valid email.')
+        .required('Please enter your email.'),
+    password: yup
+        .string()
+        .min(8, 'Must be more than 8 characters long')
+        .required('Please enter your password.'),
+    confirmPassword: yup
+        .string()
+        .min(8, 'Must be more than 8 characters long')
+        .required('Please enter your password.'),
+})
+
 export const resetPasswordValidationSchema = yup.object({
     email: yup
         .string()
